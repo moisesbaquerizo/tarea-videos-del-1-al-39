0482abc35dd2a6e527e80670ccaf30ac38d28ca6
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 12:21:01 -0500

    Create ejer30.py

---
+++ ejer30.py
@@ -0,0 +1,12 @@
+from funciones import *
+mayor=0
+numero = int(input("numero primo: "))
+while primo (numero):
+    print("suma de los diguitos: ",sumadiguito(numero))
+    diguito= int(input("diguito. "))
+    print("el",diguito,"aparece", frecuencia(numero,diguito),"veces")
+    if numero > mayor :
+        mayor = numero 
+    numero =int(input("numero primo: "))
+print("factorial de",mayor,":",factorial(mayor))
+
