9c0d8cb8b174976bd9912caab7c775c7246fa9c8
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 11:56:06 -0500

    Create ejer11.py

---
+++ ejer11.py
@@ -0,0 +1,10 @@
+nomber1=input("un nombre: ")
+nomber2=input("otro nombre: ")
+fn1nomber1=len(nomber1)-1
+fn2nomber2=len(nomber2)-1
+
+if nomber1[0]==nomber2[0] or nomber1 [fn1nomber1]==nomber2[fn2nomber2]:
+    print("coincidencia")
+else:
+    print("no hay coinsidenncia")
+
