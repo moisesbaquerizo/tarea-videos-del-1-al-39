ea7497be247b6230865aa3a5942cae7fda08ea85
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 11:51:03 -0500

    Create ejer3.py

---
+++ ejer3.py
@@ -0,0 +1,29 @@
+
+a= False
+b= True
+print(a and b)
+#------------
+print(True or (1+2))
+
+#------------
+print((len("jugar")>5) and (len("jugar")<10))
+
+#-----------
+print("alto"[2]=="t" and "x" )
+
+#------------
+print(8434893%10 and len("cafe")==3)
+
+#------------
+print(0&0 or "a"< "y")
+
+#------------
+print(True or int ("50")>=50)
+#------------
+edada=20
+print( a or edada%2==0)
+
+#-----------
+cliente = a
+print( not(cliente) and not(edada<18) )
+
