9b80b4fffafd4924f73b0fd87b9067ab059277ad
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 14:26:53 -0500

    Create ejer41.py

---
+++ ejer41.py
@@ -0,0 +1,13 @@
+from funciones import *
+sociosactivos=(1:["amanda nuñes","1223233",True ],2:["barvara molina","12276633",True ],3:["laura lopez","1222276633",True ])
+print("cargar socios ")
+sociosactivos=cargarsocios(sociosactivos)
+print("el club tiene", len(sociosactivos),"socios")
+print("registrarpago de cuota")
+numero= int(input("numero de socios "))
+sociosactivos=modificar (sociosactivos),"12234324"
+print("elimonar socio")
+nombre=input("nombre y apellido ")
+numero=numeroso(sociosactivos,nombre)
+del sociosactivos[numero]
+imprimir(sociosactivos)
