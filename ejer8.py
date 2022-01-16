40dd5d7979801de74b888845038310d01529ea4b
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 11:54:30 -0500

    Create ejer8.py

---
+++ ejer8.py
@@ -0,0 +1,6 @@
+capacidad=float(input("capacidad del tanque: "))
+klxl=float(input("rendimiento(km por litro: "))
+recoriido=float(input("kilometros totales a recorer: "))
+kmxtanque= capacidad*klxl
+print("seran nesesarios: " ,recoriido/kmxtanque,"tanques" )
+
