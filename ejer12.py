8e2e0e1cdd6884fe2f90a9f51c99738195a4544b
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 11:57:33 -0500

    Create ejer12.py

---
+++ ejer12.py
@@ -0,0 +1,9 @@
+candidato=input("candidato eleguido: ")
+if candidato.upper()=="A":
+    print("usted ha votado por el candidato rojo")
+elif candidato.upper()=="B":
+    print("usted ha votado por el candidato azul")
+elif candidato.upper()=="C":
+    print("usted ha votado por el candidato VERDE")
+else:
+    print("opcion erronea")
