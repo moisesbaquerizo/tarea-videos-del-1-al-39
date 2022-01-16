810726d1870c443bc9d486937ab8122795dd3b8e
moisesbaquerizo <85745457+moisesbaquerizo@users.noreply.github.com>
2022-01-16 11:49:58 -0500

    Create ejer2.py

---
+++ ejer2.py
@@ -0,0 +1,74 @@
+
+100/5
+print(type(100/5))
+
+#-------------------
+7/2
+3.5
+7//2
+3
+7&2
+1
+print(type(7//2))
+print(type(7&2))
+
+#-------------------
+"a"
+print(type("a"))
+#-------------------
+
+"tiza"+"-"
+print(type("tiza"+"-"))
+
+#-------------------
+"hola"[1 +1] 
+print(type("hola"[1 +1] ))
+
+#-------------------
+print(len("hola"))
+#-------------------
+int("145")
+print(type(int("145")))
+
+#-------------------
+float("145")
+print(type(float("145")))
+
+#-------------------
+str(32)
+print(type(str(32)))
+#-------------------
+177%2==0
+print(type(177%2==0 ))
+#-------------------
+n=167/10
+print(type(n ))
+#-------------------
+#------------------
+letra="a"
+print(type(letra ))
+
+#CUAL DE LAS SIGUIENTES OPERACIONES APRARESE ERROR 
+11-(4%2+10)
+"30"+2
+print("error")
+#-------------------
+"30"+2
+print("hola"(len("hola")))
+print("error")
+#-------------------
+len(124)
+print("error")
+#-------------------
+"hola"(len("fin"))
+int ("4")
+
+int(4)
+
+int("z")
+print("error")
+
+4<"f"
+print("errror")
+
+"palabra"=="rama"
